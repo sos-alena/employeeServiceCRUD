5ecb6001a5a3c079a4f5fe1220cb56264c6e4b71
sos-alena <sev18121976@gmail.om>
2024-01-27 20:44:33 +0200

    Add test

---
+++ src/test/java/employee/service/crud/models/AddressTest.java
@@ -0,0 +1,60 @@
+package employee.service.crud.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getIdTest() {
+    }
+
+    @Test
+    void getTownTest() {
+    }
+
+    @Test
+    void getStreetTest() {
+    }
+
+    @Test
+    void getCountryTest() {
+    }
+
+    @Test
+    void getPostCodeTest() {
+    }
+
+    @Test
+    void setIdTest() {
+    }
+
+    @Test
+    void setTownTest() {
+    }
+
+    @Test
+    void setStreetTest() {
+    }
+
+    @Test
+    void setCountryTest() {
+    }
+
+    @Test
+    void setPostCodeTest() {
+    }
+}
---
+++ src/test/java/employee/service/crud/models/DepartmentTest.java
@@ -0,0 +1,44 @@
+package employee.service.crud.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getIdTest() {
+    }
+
+    @Test
+    void getTitleTest() {
+    }
+
+    @Test
+    void getEmployeesTest() {
+    }
+
+    @Test
+    void setIdTest() {
+    }
+
+    @Test
+    void setTitleTest() {
+    }
+
+    @Test
+    void setEmployeesTest() {
+    }
+}
