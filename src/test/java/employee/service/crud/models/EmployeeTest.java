94ba5980b5f74fbe5270254b467b27373fe329b8
sos-alena <sev18121976@gmail.om>
2024-01-27 20:43:04 +0200

    Add test

---
+++ src/test/java/employee/service/crud/models/EmployeeTest.java
@@ -0,0 +1,108 @@
+package employee.service.crud.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getIdTest() {
+    }
+
+    @Test
+    void getFirstNameTest() {
+    }
+
+    @Test
+    void getLastNameTest() {
+    }
+
+    @Test
+    void getBirthdayTest() {
+    }
+
+    @Test
+    void getPhone_numberTest() {
+    }
+
+    @Test
+    void getAddressTest() {
+    }
+
+    @Test
+    void getPositionTest() {
+    }
+
+    @Test
+    void getEmployment_dataTest() {
+    }
+
+    @Test
+    void getData_of_dismissalTest() {
+    }
+
+    @Test
+    void getInnTest() {
+    }
+
+    @Test
+    void getDepartmentTest() {
+    }
+
+    @Test
+    void setIdTest() {
+    }
+
+    @Test
+    void setFirstNameTest() {
+    }
+
+    @Test
+    void setLastNameTest() {
+    }
+
+    @Test
+    void setBirthdayTest() {
+    }
+
+    @Test
+    void setPhone_numberTest() {
+    }
+
+    @Test
+    void setAddressTest() {
+    }
+
+    @Test
+    void setPositionTest() {
+    }
+
+    @Test
+    void setEmployment_dataTest() {
+    }
+
+    @Test
+    void setData_of_dismissalTest() {
+    }
+
+    @Test
+    void setInnTest() {
+    }
+
+    @Test
+    void setDepartmentTest() {
+    }
+}
